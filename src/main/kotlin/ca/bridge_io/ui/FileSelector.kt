ae4e89f9140eb39627e193ddbb980c224db41468
Tyluur <contact@tyluur.com>
2023-08-22 12:30:48 -0400

    feat(utils): Refactor Utility class with coroutine support
    - Enhance the unzip function to utilize ZipInputStream, improving memory efficiency
    - Add suspend function to extract files asynchronously with coroutine support
    - Implement progress monitoring for file extraction
    - Improve exception handling and logging
    - Include detailed documentation for all functions and properties
    - Remove redundant code and dependencies

--- src/main/kotlin/ca/bridge_io/ui/FileSelector.kt
+++ src/main/kotlin/ca/bridge_io/ui/FileSelector.kt
@@ -1,13 +1,8 @@
 package ca.bridge_io.ui
 
+import ca.bridge_io.utils.Utility.extractFile
 import com.github.michaelbull.logging.InlineLogger
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import net.lingala.zip4j.ZipFile
-import net.lingala.zip4j.progress.ProgressMonitor
-import java.io.File
 import javax.swing.JFileChooser
 import javax.swing.filechooser.FileNameExtensionFilter
 
@@ -39,48 +34,6 @@ class FileSelector {
 
 		if (option == JFileChooser.APPROVE_OPTION) {
 			runBlocking { extractFile(file) }
-		}
-	}
-
-	/**
-	 * Extracts the selected file to a specified directory.
-	 *
-	 * @param file The file to extract.
-	 */
-	private suspend fun extractFile(file: File) = withContext(Dispatchers.IO) {
-		val extractDirectory = File(file.absolutePath)
-		logger.info { "Approve Option selected... Extracting to directory [$extractDirectory]" }
-		val zipFile = ZipFile(file)
-		val outputFolder = "${zipFile.file.parentFile}/${zipFile.file.name.substringBefore(".")}"
-		val outputFolderFile = File(outputFolder)
-
-		if (!outputFolderFile.exists() && !outputFolderFile.mkdirs()) {
-			logger.error { "Unable to create export directory" }
-			throw FileExtractionException("Unable to create export directory")
-		}
-
-		logger.debug { "Constructed export directory" }
-
-		try {
-			zipFile.extractAll(outputFolder)
-			monitorExtractionProgress(zipFile)
-			logger.info { "Successfully Extracted Files from ${zipFile.file.absoluteFile}" }
-		} catch (e: Exception) {
-			logger.error { "Failed to extract files: ${e.message}" }
-			throw FileExtractionException("Failed to extract files", e)
-		}
-	}
-
-	/**
-	 * Monitors the extraction progress.
-	 *
-	 * @param zipFile The ZipFile to monitor.
-	 */
-	private suspend fun monitorExtractionProgress(zipFile: ZipFile) = withContext(Dispatchers.Default) {
-		val monitor = zipFile.progressMonitor
-		while (!monitor.state.equals(ProgressMonitor.State.READY)) {
-			logger.debug { "Percentage Complete = [${monitor.percentDone}], File = [${monitor.fileName}], Task = [${monitor.currentTask}]" }
-			delay(100)
 		}
 	}
 
--- src/main/kotlin/ca/bridge_io/utils/Utility.kt
+++ src/main/kotlin/ca/bridge_io/utils/Utility.kt
@@ -1,6 +1,13 @@
 package ca.bridge_io.utils
 
+import ca.bridge_io.ui.FileSelector
+import com.github.michaelbull.logging.InlineLogger
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import net.lingala.zip4j.ZipFile
 import net.lingala.zip4j.io.inputstream.ZipInputStream
+import net.lingala.zip4j.progress.ProgressMonitor
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
@@ -13,31 +20,79 @@ import java.io.FileOutputStream
  */
 object Utility {
 
-    /**
-     * Unzips a given ZIP file using an optional password.
-     *
-     * @param input The ZIP file to be unzipped.
-     * @param password The password used to unlock the ZIP file (if encrypted).
-     * @throws Exception if the ZIP file is corrupted or an incorrect password is provided.
-     */
-    fun unzip(input: File, password: CharArray) {
-        FileInputStream(input).use { fileInputStream ->
-            val readBuffer = ByteArray(4096)
+	/**
+	 * Unzips a given ZIP file using an optional password.
+	 *
+	 * @param input The ZIP file to be unzipped.
+	 * @param password The password used to unlock the ZIP file (if encrypted).
+	 * @throws Exception if the ZIP file is corrupted or an incorrect password is provided.
+	 */
+	fun unzip(input: File, password: CharArray) {
+		FileInputStream(input).use { fileInputStream ->
+			val readBuffer = ByteArray(4096)
 
-            ZipInputStream(fileInputStream, password).use { zipInputStream ->
-                while (true) {
-                    val localFileHeader = zipInputStream.nextEntry ?: break
-                    val extractedFile = File(localFileHeader.fileName)
+			ZipInputStream(fileInputStream, password).use { zipInputStream ->
+				while (true) {
+					val localFileHeader = zipInputStream.nextEntry ?: break
+					val extractedFile = File(localFileHeader.fileName)
 
-                    FileOutputStream(extractedFile).use { outputStream ->
-                        while (true) {
-                            val readLen = zipInputStream.read(readBuffer)
-                            if (readLen == -1) break
-                            outputStream.write(readBuffer, 0, readLen)
-                        }
-                    }
-                }
-            }
-        }
-    }
+					FileOutputStream(extractedFile).use { outputStream ->
+						while (true) {
+							val readLen = zipInputStream.read(readBuffer)
+							if (readLen == -1) break
+							outputStream.write(readBuffer, 0, readLen)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Extracts the selected file to a specified directory.
+	 *
+	 * @param file The file to extract.
+	 */
+	suspend fun extractFile(file: File) = withContext(Dispatchers.IO) {
+		val extractDirectory = File(file.absolutePath)
+		logger.info { "Approve Option selected... Extracting to directory [$extractDirectory]" }
+		val zipFile = ZipFile(file)
+		val outputFolder = "${zipFile.file.parentFile}/${zipFile.file.name.substringBefore(".")}"
+		val outputFolderFile = File(outputFolder)
+
+		if (!outputFolderFile.exists() && !outputFolderFile.mkdirs()) {
+			logger.error { "Unable to create export directory" }
+			throw FileSelector.FileExtractionException("Unable to create export directory")
+		}
+
+		logger.debug { "Constructed export directory" }
+
+		try {
+			zipFile.extractAll(outputFolder)
+			monitorExtractionProgress(zipFile)
+			logger.info { "Successfully Extracted Files from ${zipFile.file.absoluteFile}" }
+		} catch (e: Exception) {
+			logger.error { "Failed to extract files: ${e.message}" }
+			throw FileSelector.FileExtractionException("Failed to extract files", e)
+		}
+	}
+
+	/**
+	 * Monitors the extraction progress.
+	 *
+	 * @param zipFile The ZipFile to monitor.
+	 */
+	private suspend fun monitorExtractionProgress(zipFile: ZipFile) = withContext(Dispatchers.Default) {
+		val monitor = zipFile.progressMonitor
+		while (!monitor.state.equals(ProgressMonitor.State.READY)) {
+			logger.debug { "Percentage Complete = [${monitor.percentDone}], File = [${monitor.fileName}], Task = [${monitor.currentTask}]" }
+			delay(100)
+		}
+	}
+
+	/**
+	 * The instance of the logger for this class
+	 */
+	private val logger = InlineLogger()
+
 }
