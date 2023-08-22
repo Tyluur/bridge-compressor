1aae21e8e9c1561419e0b218139fd3af73c5db77
Tyluur <contact@tyluur.com>
2023-08-22 00:12:06 -0400

    fix: update logger to kotlin-inline-logger » 1.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,25 +19,25 @@ repositories {
 }
 
 dependencies {
-	//Logging
 	implementation("org.slf4j:slf4j-api:2.0.5")
 	implementation("ch.qos.logback:logback-classic:1.2.9")
-	implementation(
-		group = "com.michael-bull.kotlin-inline-logger", name = "kotlin-inline-logger-jvm", version = "1.0.2"
-	)
-
-	// Unzip Library
-	implementation(group = "net.lingala.zip4j", name = "zip4j", version = "2.11.1")
-
-	// Co Routine Usage
-	implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.3.7")
-
+	implementation("net.lingala.zip4j", "zip4j", "2.11.1")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+	implementation("com.michael-bull.kotlin-inline-logger:kotlin-inline-logger:1.0.5")
+	runtimeOnly("com.michael-bull.kotlin-inline-logger:kotlin-inline-logger-jvm:1.0.5")
 	testImplementation(kotlin("test"))
+}
+
+tasks.jar {
+	manifest {
+		attributes["Main-Class"] = "ca.bridge-io.BootstrapKt"
+	}
+	from(sourceSets.main.get().output)
 }
 
 java {
 	toolchain {
-		languageVersion.set(JavaLanguageVersion.of(11)) // Change this to match your project's target JVM version
+		languageVersion.set(JavaLanguageVersion.of(11))
 	}
 }
 
